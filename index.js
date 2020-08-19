dde9de138fe9ff5f2fde2ed2a3a5a52148d0a64a
Alexa Gamil <alexis_gamil99@yahoo.com>
2020-08-19 22:32:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ function theBeatlesPlay(musicians, instruments) {
   return newArray;
 }
 
+function johnLennonFacts(facts){
+  var i = 0;
+  while(i < facts.length) {
+    facts.push(`${facts[i]}!!!`)
+    i++;
+  }
+  return facts;
+}
