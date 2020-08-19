e3735a4a6c606a16eb11692a9ddc45d3330166d7
Alexa Gamil <alexis_gamil99@yahoo.com>
2020-08-19 21:58:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
-  
+  var newArray =[];
+  for(var i = 0 ; i < musicians.length && instruments.length; i++){
+    newArray.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return newArray;
 }
